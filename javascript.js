2f5b4998666ab1893193e64c162dec5a4ee8e0c7
jebsmartt <jeb.smartt@gmail.com>
2023-06-20 20:48:06 -0700

    Complete assignment for JS Fundamentals Pt 1

--- javascript.js
+++ javascript.js
@@ -12,3 +12,14 @@
 // let user;
 
 console.log(23 + 97)
+
+console.log(5 + 4 + 3 + 2 + 1 + 0)
+
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+
+const max = 57;
+let actual = max -13;
+let percentage = actual / max
